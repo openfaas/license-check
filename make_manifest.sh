1a5e6125e66e5111c74f90bd7fde14e99f8b2374
Lucas Roesler <roesler.lucas@gmail.com>
2020-01-28 22:03:11 +0100

    Add docker image deployment to the travis config
    **What**
    - Update the docker images to use Go 1.13
    - Add windows build
    - Add bash scripts for creating arch specific docker builds
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

---
+++ make_manifest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DOCKER_NS=${DOCKER_NS:-openfaas}
+TAG=${TRAVIS_TAG:-latest}
+NAME=license-check
+
+docker manifest create $DOCKER_NS/$NAME:$TAG \
+  $DOCKER_NS/$NAME:$TAG-darwin \
+  $DOCKER_NS/$NAME:$TAG-x86_64 \
+  $DOCKER_NS/$NAME:$TAG-armhf \
+  $DOCKER_NS/$NAME:$TAG-arm64 \
+  $DOCKER_NS/$NAME:$TAG-windows
+
+docker manifest annotate $DOCKER_NS/$NAME:$TAG --arch arm $DOCKER_NS/$NAME:$TAG-darwin
+docker manifest annotate $DOCKER_NS/$NAME:$TAG --arch arm $DOCKER_NS/$NAME:$TAG-armhf
+docker manifest annotate $DOCKER_NS/$NAME:$TAG --arch arm64 $DOCKER_NS/$NAME:$TAG-arm64
+docker manifest annotate $DOCKER_NS/$NAME:$TAG --os windows $DOCKER_NS/$NAME:$TAG-windows
+
+docker manifest push $DOCKER_NS/$NAME:$TAG
